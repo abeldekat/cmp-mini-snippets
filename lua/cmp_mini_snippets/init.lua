37894ad61af8b5de3a4a5a267d49f186cdc8f0c2
abeldekat <abel@nomail.com>
2024-12-31 19:08:59 +0100

    the snippets show up and can be expanded. Todo: the documentation

--- lua/cmp_mini_snippets/init.lua
+++ lua/cmp_mini_snippets/init.lua
@@ -1,90 +1,90 @@
+--[[
+:h cmp-develop
+--]]
+
 local cmp = require("cmp")
 local util = require("vim.lsp.util")
 
 local source = {}
 
----@class cmp_mini_snippets.Option
----@field public dummy boolean
-
----@type cmp_mini_snippets.Option
-local defaults = {
-  dummy = false,
-}
-
--- copied from cmp-luasnip:
--- the options are being passed via cmp.setup.sources, e.g.
--- require('cmp').setup { sources = { { name = 'mini_snippets', opts = {...} } } }
-local function init_options(params)
-  params.option = vim.tbl_deep_extend("keep", params.option, defaults)
-  vim.validate({
-    dummy = { params.option.dummy, "boolean" },
-  })
-end
-
--- copied from cmp-luasnip:
-local function get_documentation(snip, data)
-  local header = (snip.name or "") .. " _ `[" .. data.filetype .. "]`\n"
-  local docstring = { "", "```" .. vim.bo.filetype, snip:get_docstring(), "```" }
-  local documentation = { header .. "---", (snip.dscr or ""), docstring }
+-- Creates a markdown representation f the snippet
+---@return string
+local function get_documentation(snippet)
+  local header = (snippet.label or "") .. "\n"
+  local docstring = { "", "```", snippet.body, "```" }
+  local documentation = { header .. "---", (snippet.desc or ""), docstring }
   documentation = util.convert_input_to_markdown_lines(documentation)
-  documentation = table.concat(documentation, "\n")
 
-  doc_cache[data.filetype] = doc_cache[data.filetype] or {}
-  doc_cache[data.filetype][data.snip_id] = documentation
-  return documentation
+  return table.concat(documentation, "\n")
 end
 
 source.new = function() return setmetatable({}, { __index = source }) end
 
--- copied from cmp-luasnip:
-source.get_keyword_pattern = function()
-  vim.print("get_keyword_pattern")
+---Return the keyword pattern for triggering completion (optional).
+---If this is omitted, nvim-cmp will use a default keyword pattern. See |cmp-config.completion.keyword_pattern|.
+---@return string
+source.get_keyword_pattern = function() -- same as cmp-luasnip!
   return "\\%([^[:alnum:][:blank:]]\\|\\w\\+\\)"
 end
 
--- Not in cmp-path or cmp-buffer:
--- function source:get_debug_name() return "mini.snippets" end
+-- Copied from :h nvim-cmp as a reminder...
+---Return trigger characters for triggering completion (optional).
+-- function source:get_trigger_characters() return { "." } end
 
-function source:complete(params, callback)
-  vim.print("source complete")
-  vim.print(params)
+---Return whether this source is available in the current context or not (optional).
+---@return boolean
+function source:is_available() -- lazy-loading...
+  local ok, _ = pcall(require, "mini.snippets")
+  return ok
+end
 
-  init_options(params)
-
+-- TODO: cmp-luasnip caches the produced list by ft
+--
+---Invoke completion (required).
+-- -@param params cmp.SourceCompletionApiParams
+---@param callback fun(response: lsp.CompletionResponse|nil)
+function source:complete(_, callback)
   local items = {}
+  local snippets = MiniSnippets.expand({ match = false, insert = false })
+  if not snippets then return items end
 
+  for _, snippet in ipairs(snippets) do
+    items[#items + 1] = {
+      word = snippet.prefix,
+      label = snippet.prefix,
+      kind = cmp.lsp.CompletionItemKind.Snippet,
+      data = {
+        -- cmp-luasnip also has priority, filetype, show_condition and auto...
+        snippet = snippet, -- cmp-luasnip only stores the snippet-id...
+      },
+    }
+  end
   callback(items)
 end
 
--- copied from cmp-luasnip:
-function source:resolve(completion_item, callback)
-  vim.print("source complete")
-  vim.print(completion_item)
-
-  -- local item_snip_id = completion_item.data.snip_id
-  -- local snip = require("luasnip").get_id_snippet(item_snip_id)
-  -- local doc_itm = doc_cache[completion_item.data.filetype] or {}
-  -- doc_itm = doc_itm[completion_item.data.snip_id] or get_documentation(snip, completion_item.data)
-  -- completion_item.documentation = {
-  --   kind = cmp.lsp.MarkupKind.Markdown,
-  --   value = doc_itm,
-  -- }
+-- TODO: cmp-luasnip caches the produced documentation as well
+--
+---Resolve completion item (optional). This is called right before the completion is about to be displayed.
+---Useful for setting the text shown in the documentation window (`completion_item.documentation`).
+---@param completion_item lsp.CompletionItem
+---@param callback fun(completion_item: lsp.CompletionItem|nil)
+function source:resolve(completion_item, callback) -- modified from cmp-luasnip:
+  local snippet = completion_item.data.snippet
+  completion_item.documentation = {
+    kind = cmp.lsp.MarkupKind.Markdown,
+    value = get_documentation(snippet),
+  }
   callback(completion_item)
 end
 
--- not in cmp-path or cmp-buffer!
+-- TODO: cmp-luasnip has a fairly extensive implementation. Keep it simple for now.
+-- TODO: Maybe this can be optimized by passing the choosen snippet to MiniSnippets.expand...
+--
+---Executed after the item was selected.
+---@param completion_item lsp.CompletionItem
+---@param callback fun(completion_item: lsp.CompletionItem|nil)
 function source:execute(completion_item, callback)
-  vim.print("source execute")
-  vim.print(completion_item)
-
-  -- -- text cannot be cleared before, as TM_CURRENT_LINE and
-  -- -- TM_CURRENT_WORD couldn't be set correctly.
-  -- require("luasnip").snip_expand(snip, {
-  -- 	-- clear word inserted into buffer by cmp.
-  -- 	-- cursor is currently behind word.
-  -- 	clear_region = clear_region,
-  -- 	expand_params = expand_params,
-  -- })
+  MiniSnippets.expand()
   callback(completion_item)
 end
 
